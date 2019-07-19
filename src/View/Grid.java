e76c236e8e349c5d247760c9826f4d8d479389e5
Lunga MAKWAKWA <lmakwakw@c4r14s4.wethinkcode.co.za>
2019-07-19 14:41:05 +0200

    grid created

---
+++ src/View/Grid.java
@@ -0,0 +1,20 @@
+package View;
+
+public class Grid {
+/*
+    public int level  = hero.getLevel();
+
+    public int GridSize = (level - 1) * 5 + 10 - (level % 2);
+
+
+    for(int i = 0; i < GridSize; ++i) {
+
+        for (int j = 0; j < GridSize; ++j) {
+//            /System.out.print(this.Layout[i][j]);
+        }
+        System.out.println("");
+    }
+*/
+
+
+}
--- src/swingy.java
+++ src/swingy.java
@@ -4,15 +4,37 @@ import View.CalculatorView;
 
 public class swingy {
 
+
+
+    public int map = (level - 1) * 5 + 10 - (level % 2);
+
+
     public static void main(String[] args) {
 
-        CalculatorView theView = new CalculatorView();
+        int map = 8;
 
-        CalculatorModel theModel = new CalculatorModel();
+        System.out.printf("MAP %d Map1 %d", map, map);
+        System.out.println();
+        for (int i = 0; i < map; i++) {
+            for (int j = 0; j < map; j++) {
 
-        CalculatorController theController = new CalculatorController(theView,theModel);
+               if (j == 4 && i == 4)
+                    System.out.print("H. ");
+//                else if (map[i][j])
+ //                   System.out.print("* ");
+                else
+                    System.out.print(" . ");
+            }
+            System.out.println();
+        }
 
-        theView.setVisible(true);
+        //CalculatorView theView = new CalculatorView();
+
+        //CalculatorModel theModel = new CalculatorModel();
+
+        //CalculatorController theController = new CalculatorController(theView,theModel);
+
+        //theView.setVisible(true);
 
     }
 }
